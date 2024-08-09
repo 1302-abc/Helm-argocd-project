9e0b1685d00c7d0ddbcbde79f330420bca1b3ac7
Ishita Vinayak <ishivinayak17@gmail.com>
2024-08-09 03:50:24 +0000

    initial commit

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,15 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return jsonify(message="Hello from the backend!")
+def index():
+    return jsonify({
+        'message': 'Hello from the backend!',
+        'data1': 'Brundha',
+        'data2': 'Swapnil',
+        'data3': 'Manuja',
+        'data4': 'Sathana',
+        'data5': 'Ishita',  
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
